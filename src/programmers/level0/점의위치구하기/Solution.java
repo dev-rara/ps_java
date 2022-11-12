0cb405cdb329073c36cd3d7d80dd771596469906
rara-dev <kksr1219@gmail.com>
2022-11-12 22:52:20 +0900

    2022.11.12

---
+++ src/programmers/level0/점의위치구하기/Solution.java
@@ -0,0 +1,20 @@
+package programmers.level0.점의위치구하기;
+
+public class Solution {
+
+	public int solution(int[] dot) {
+		int answer = 0;
+
+		if (dot[0] > 0 && dot[1] > 0) {
+			answer = 1;
+		} else if (dot[0] < 0 && dot[1] > 0) {
+			answer = 2;
+		} else if (dot[0] < 0 && dot[1] < 0) {
+			answer = 3;
+		} else if (dot[0] > 0 && dot[1] < 0) {
+			answer = 4;
+		}
+		return answer;
+	}
+
+}

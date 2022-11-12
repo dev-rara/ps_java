18fa726f73f7281144721fcccb9d55ea91cf7cca
rara-dev <kksr1219@gmail.com>
2022-11-12 22:12:33 +0900

    2022.11.12

---
+++ src/programmers/level0/두수의곱/Solution.java
@@ -0,0 +1,7 @@
+package programmers.level0;
+
+public class Solution {
+	public int solution(int num1, int num2) {
+		return num1 * num2;
+	}
+}
---
+++ src/programmers/level0/두수의차/Solution.java
@@ -0,0 +1,8 @@
+package programmers.level0.두수의차;
+
+public class Solution {
+	public int solution(int num1, int num2) {
+		return num1 - num2;
+	}
+
+}

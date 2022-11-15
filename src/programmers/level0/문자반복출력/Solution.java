5194146d254ba0d2ea106d7fd61661776c42d317
rara-dev <kksr1219@gmail.com>
2022-11-15 18:40:45 +0900

    2022.11.15

---
+++ src/programmers/level0/문자반복출력/Solution.java
@@ -0,0 +1,14 @@
+package programmers.level0.문자반복출력;
+
+public class Solution {
+	public String solution(String my_string, int n) {
+		String answer = "";
+		String[] arr = my_string.split("");
+
+		for(int i = 0; i < my_string.length(); i++) {
+			answer += arr[i].repeat(n);
+		}
+		return answer;
+	}
+
+}

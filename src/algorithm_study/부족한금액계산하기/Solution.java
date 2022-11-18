6a79e97dc91b69d9473316b6ad2b35c917a4b00c
rara-dev <kksr1219@gmail.com>
2022-11-18 14:35:11 +0900

    2022.11.18

---
+++ src/algorithm_study/부족한금액계산하기/Solution.java
@@ -0,0 +1,14 @@
+package algorithm_study.부족한금액계산하기;
+
+public class Solution {
+
+	public long solution(int price, int money, int count) {
+		long answer = 0;
+		for (int i = 1; i <= count; i++) {
+			answer += price * i;
+		}
+		answer = money - answer;
+
+		return answer < 0 ? Math.abs(answer) : 0;
+	}
+}

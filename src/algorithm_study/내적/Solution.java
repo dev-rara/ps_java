1448331c2f398770c2810d81bb5c33cd6b625581
rara-dev <kksr1219@gmail.com>
2022-11-18 16:11:33 +0900

    2022.11.18

---
+++ src/algorithm_study/내적/Solution.java
@@ -0,0 +1,13 @@
+package algorithm_study.내적;
+
+public class Solution {
+
+	public int solution(int[] a, int[] b) {
+		int answer = 0;
+
+		for (int i = 0; i < a.length; i++) {
+			answer += a[i] * b[i];
+		}
+		return answer;
+	}
+}

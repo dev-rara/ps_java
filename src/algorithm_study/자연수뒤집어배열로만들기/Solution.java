232d5b949fc75b78d146e29e610189735c43270b
rara-dev <kksr1219@gmail.com>
2022-11-19 14:56:25 +0900

    2022.11.18

---
+++ src/algorithm_study/자연수뒤집어배열로만들기/Solution.java
@@ -0,0 +1,17 @@
+package algorithm_study.자연수뒤집어배열로만들기;
+
+public class Solution {
+
+	public int[] solution(long n) {
+		String str = String.valueOf(n);
+		int len = str.length();
+		int[] answer = new int[len];
+
+		for (int i = 0; i < str.length(); i++) {
+			String temp = String.valueOf(str.charAt(len - 1 - i));
+			answer[i] = Integer.parseInt(temp);
+		}
+		return answer;
+	}
+
+}

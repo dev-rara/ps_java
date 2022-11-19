175b474e80cfea6bfdbe1545901bfd7f8d621cc8
rara-dev <kksr1219@gmail.com>
2022-11-19 19:21:46 +0900

    2022.11.19

--- src/algorithm_study/하샤드수/Solution.java
+++ src/algorithm_study/하샤드수/Solution.java
@@ -1,5 +1,20 @@
 package algorithm_study.하샤드수;
 
 public class Solution {
+	public boolean solution(int x) {
+		boolean answer = true;
+		int num = x;
+		int sum = 0;
 
+		while (num != 0) {
+			sum += (num % 10);
+			num /= 10;
+		}
+
+		if (x % sum != 0) {
+			answer = false;
+		}
+
+		return answer;
+	}
 }

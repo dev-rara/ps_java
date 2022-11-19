f42d57ac19fe84948a5991a61f4a0a9dda27dac4
rara-dev <kksr1219@gmail.com>
2022-11-19 17:45:58 +0900

    2022.11.19

---
+++ src/algorithm_study/콜라츠추측/Solution.java
@@ -0,0 +1,35 @@
+package algorithm_study.콜라츠추측;
+
+public class Solution {
+
+	public int solution(int num) {
+		int answer = 0;
+		long number = (long) num;
+
+		if (num == 1) {
+			return 0;
+		}
+
+		while (number != 1) {
+			if (answer == 500) {
+				return -1;
+			}
+
+			if (number % 2 == 0) {
+				number /= 2;
+				answer++;
+			} else {
+				number = (long) (number * 3) + 1;
+				answer++;
+			}
+		}
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Solution sol = new Solution();
+		System.out.println(sol.solution(626331));
+	}
+
+}

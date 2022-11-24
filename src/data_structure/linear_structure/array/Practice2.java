4a990b48c6a5e8e24c062d3fedc41cc47e95bbe3
rara-dev <kksr1219@gmail.com>
2022-11-24 10:30:05 +0900

    study array

---
+++ src/data_structure/linear_structure/array/Practice2.java
@@ -0,0 +1,27 @@
+package data_structure.linear_structure.array;// Practice2
+// 배열 arr 에서 target 에 해당하는 값의 인덱스를 출력
+// 해당 값이 여러 개인 경우 가장 큰 인덱스 출력
+
+// 입출력 예시)
+// 배열 arr: 1, 1, 100, 1, 1, 1, 100
+// target: 100
+// 결과: 6
+
+public class Practice2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 100, 1, 1, 1, 100};
+        int target = 100;
+        int targetIndex = -1;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                if (i > targetIndex) {
+                    targetIndex = i;
+                }
+            }
+        }
+        if (targetIndex >= 0) {
+            System.out.println(targetIndex);
+        }
+    }
+}

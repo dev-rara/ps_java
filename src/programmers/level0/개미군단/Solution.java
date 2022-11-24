9cfc39b79cd3da6bce86fdc7acc328941ed73b33
rara-dev <kksr1219@gmail.com>
2022-11-24 22:45:18 +0900

    2022.11.24

---
+++ src/programmers/level0/개미군단/Solution.java
@@ -0,0 +1,15 @@
+package programmers.level0.개미군단;
+
+public class Solution {
+
+	public int solution(int hp) {
+		int answer = 0;
+
+		for (int i = 5; i > 0; i -= 2) {
+			answer += hp / i;
+			hp %= i;
+		}
+
+		return answer;
+	}
+}

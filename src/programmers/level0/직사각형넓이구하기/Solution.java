f685f03c8b282af780943789fb964ed29f5d5931
rara-dev <kksr1219@gmail.com>
2022-12-20 14:52:19 +0900

    2022.12.20

--- src/programmers/level0/직사각형넓이구하기/Solution.java
+++ src/programmers/level0/직사각형넓이구하기/Solution.java
@@ -5,12 +5,7 @@ import java.util.Comparator;
 
 public class Solution {
 	public int solution(int[][] dots) {
-		Arrays.sort(dots, new Comparator<int[]>() {
-			@Override
-			public int compare(int[] o1, int[] o2) {
-				return o1[0] - o2[0];
-			}
-		});
+		Arrays.sort(dots,Comparator.comparingInt((int[] o) -> o[0]));
 
 		int x = Math.abs(dots[1][1] - dots[0][1]);
 		int y = Math.abs(dots[2][0] - dots[0][0]);

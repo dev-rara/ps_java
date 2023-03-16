f148c9e611d3e1129a1c713d951044b60f60b863
rara-dev <kksr1219@gmail.com>
2023-03-17 00:00:11 +0900

    2023.03.16

--- src/programmers/level0/푸드파이트대회/Solution.java
+++ src/programmers/level0/푸드파이트대회/Solution.java
@@ -2,14 +2,12 @@ package programmers.level0.푸드파이트대회;
 
 public class Solution {
 	public String solution(int[] food) {
-
 		StringBuilder sb = new StringBuilder();
-
 
 		for(int i = 1; i < food.length; i++) {
 			int division = food[i] / 2;
 			sb.append(String.valueOf(i).repeat(Math.max(0, division)));
 		}
-		return sb.toString() + "0" + sb.reverse().toString();
+		return sb + "0" + sb.reverse();
 	}
 }
